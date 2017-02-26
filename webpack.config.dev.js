04feb21219b329d21945d66c9fbf9e6220a2a30e
Matt Dionis <mattdionis@MattBook-Pro.local>
2017-02-26 11:26:37 -0500

    Upgrade webpack to 2.2.1

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,14 @@
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
 
 export default {
-  debug: true,
-  devtool: 'inline-source-map',
-  noInfo: false,
+  resolve: {
+    extensions: ['*', '.js', '.json']
+  },
+  devtool: 'eval-source-map',
   entry: [
-    path.resolve(__dirname, 'src/index')
+    path.resolve(__dirname, 'src/index.js')
   ],
   target: 'web',
   output: {
@@ -15,16 +17,30 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
-    // Create HTML file that includes reference to bundled JS.
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      __DEV__: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
       inject: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true
     })
   ],
   module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, loaders: ['style-loader','css-loader?sourceMap']}
     ]
   }
-}
+};
