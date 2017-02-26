64ea1d5364893bd372c2ee1d53578d37294d30ff
Matt Dionis <mattdionis@MattBook-Pro.local>
2017-02-26 10:46:01 -0500

    Remove travis, appveyor and several scripts

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -46,10 +46,7 @@ export default {
         minifyCSS: true,
         minifyURLs: true
       },
-      inject: true,
-      // Properties you define here are available in index.html
-      // using htmlWebpackPlugin.options.varName
-      trackJSToken: 'INSERT YOUR TOKEN HERE'
+      inject: true
     }),
 
     // Eliminate duplicate packages when generating bundle
