adad328b255b0d8aa904d9dd1e379a4d9ff424de
Matt Dionis <mattdionis@MattBook-Pro.local>
2017-02-26 20:45:12 -0500

    Test ui-router hello world setup

--- src/app/app.js
+++ src/app/app.js
@@ -8,9 +8,24 @@ import AppComponent from './app.component';
 angular.module('app', [
     uirouter
   ])
-  .config($locationProvider => {
+  .config(($locationProvider, $stateProvider) => {
     'ngInject';
 
     $locationProvider.html5Mode(true).hashPrefix('!');
+
+    var helloState = {
+      name: 'hello',
+      url: '/hello',
+      template: '<h3>hello world!</h3>'
+    }
+
+    var aboutState = {
+      name: 'about',
+      url: '/about',
+      template: '<h3>Its the UI-Router hello world app!</h3>'
+    }
+
+    $stateProvider.state(helloState);
+    $stateProvider.state(aboutState);
   })
   .component('app', AppComponent);
